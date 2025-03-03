a93538eafbbd73a55a776357d9393fd8126c7f99
Track404 <111713000+Track404@users.noreply.github.com>
2025-03-03 17:12:07 +0100

    creation of groupUser controller

---
+++ controllers/groupUserController.js
@@ -0,0 +1,47 @@
+const groupUserModel = require('../models/groupUserModel');
+
+async function createGroupUser(req, res) {
+  const groupUser = await groupUserModel.createGroupUser(
+    Number(req.params.id),
+    Number(req.params.groupId)
+  );
+  res.json({ groupUser: groupUser, message: 'Group User Created' });
+}
+
+async function getUniqueGroupUserById(req, res) {
+  const groupUser = await groupUserModel.getUniqueGroupUserById(
+    Number(req.params.id)
+  );
+  res.json({
+    groupUser: groupUser,
+    message: `Get Group User ${req.params.id}`,
+  });
+}
+
+async function getAllGroupUsers(req, res) {
+  const groupUser = await groupUserModel.getAllGroupUsers();
+  res.json({ groupUser: groupUser, message: `All Groups Users` });
+}
+
+async function updateGroupUser(req, res) {
+  const groupUser = await groupUserModel.updateGroupUser(
+    Number(req.params.groupId)
+  );
+  res.json({ groupUser: groupUser, message: 'Group User Updated' });
+}
+
+async function deleteGroupUser(req, res) {
+  const groupUser = await groupUserModel.deleteGroupUser(Number(req.params.id));
+  res.json({
+    groupUser: groupUser,
+    message: `Delete Group User ${req.params.id}`,
+  });
+}
+
+module.exports = {
+  createGroupUser,
+  getUniqueGroupUserById,
+  getAllGroupUsers,
+  updateGroupUser,
+  deleteGroupUser,
+};
