acc8b74ece03a0438ccce16e6426322bf3c37853
Track404 <111713000+Track404@users.noreply.github.com>
2025-03-04 13:09:14 +0100

    creation of messageModel

---
+++ models/messageModel.js
@@ -0,0 +1,56 @@
+const prisma = require('../prisma/client');
+
+async function createMessage(content, userId, chatId, groupId) {
+  const message = await prisma.message.create({
+    data: {
+      content: content,
+      userId: userId,
+      chatId: chatId,
+      groupId: groupId,
+    },
+  });
+  return message;
+}
+
+async function getMessageById(id) {
+  const message = await prisma.message.findUnique({
+    where: {
+      id: id,
+    },
+  });
+  return message;
+}
+
+async function getAllMessage() {
+  const messages = await prisma.message.findMany();
+  return messages;
+}
+
+async function updateMessage(id, content) {
+  const message = await prisma.message.update({
+    where: {
+      id: id,
+    },
+    data: {
+      content: content,
+    },
+  });
+  return message;
+}
+
+async function deleteMessage(id) {
+  const message = await prisma.message.delete({
+    where: {
+      id: id,
+    },
+  });
+  return message;
+}
+
+module.exports = {
+  createMessage,
+  getMessageById,
+  getAllMessage,
+  updateMessage,
+  deleteMessage,
+};
